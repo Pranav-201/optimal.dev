4f1421f182379bef94987b79669bace54820e864
Pranav Amrutkar <pranav8175@gmail.com>
2026-08-12 17:03:36 +0530

    solve vite/proxy problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
   try {
     await syncDb();
     console.log("DB connected & synced");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
   } catch (err) {
     console.error("Failed to start server:", err);
     process.exit(1);
